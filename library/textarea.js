430227e8719641228c896bf82f65c1d0e790244e
ABWEBIT <alexander.borzy@gmail.com>
2025-01-29 07:49:08 +0200

    update

--- library/textarea.js
+++ library/textarea.js
@@ -1,49 +1,80 @@
-class TextInput extends HTMLElement{
+class InputText extends HTMLElement{
   constructor(){
     super();
     this.attachShadow({mode:'open'});
-    this._required = true;
-    this._editable = true;
-    this._label = 'Label';
-    this._helper = '';
-    this.addEventListener('input',()=>this.changeInput());
+    this.required = false;
+    this.disabled = false;
+    this.label = 'Label';
+    this.helper = '';
+    this.render();
   }
 
-  renderElement(){
+  render(){
+    // style
+    let style =`
+      <style>
+      :host{
+        position:relative;
+        display:inline-flex;
+        flex-direction:column;
+        row-gap:5px;}
+
+      :host > label{
+        color:rgb(255,255,255)}
+
+      :host > .wrap{
+        overflow:hidden;
+        border-radius:10px;
+        padding-left:10px;
+        padding-right:10px;
+        background-color:rgb(255,255,255);}
+
+      :host > .wrap > .input{
+        max-height:100px;
+        display:block;
+
+        border:0;
+        outline:none;}
+
+      :host > .wrap > .input::-webkit-scrollbar{width:6px;}
+      :host > .wrap > .input::-webkit-scrollbar-track{background-color:transparent;}
+      :host > .wrap > .input::-webkit-scrollbar-thumb{
+        border-radius:6px;
+        background-color:rgb(115,115,115);}
+
+      </style>`;
+
     // label
-    if(this._label && typeof this._label === 'string'){
-      this._labelHTML = `
-        <label>${this._label}${this._required === true ? ' *' : ''}</label>`;
+    if(this.label && typeof this.label === 'string'){
+      this.labelHTML = `
+        <label>${this.label}${this.required === true ? '*' : ''}</label>`;
     }
-    else this._labelHTML = '';
+    else this.labelHTML = '';
 
     // input
-    this._inputHTML = `<div contenteditable="true"></div>`;
+    this.inputHTML = `
+      <div class="wrap">
+        <div class="input" contenteditable="true"></div>
+      </div>`;
 
     // helper
-    this._helperHTML = `<div class="helper"></div>`;
+    this.helperHTML = `<div class="helper"></div>`;
 
     // build
-    this.innerHTML = `
-      ${this._labelHTML}
-      ${this._inputHTML}
-      ${this._helperHTML}
+    this.shadowRoot.innerHTML = `
+      ${style}
+      ${this.labelHTML}
+      ${this.inputHTML}
+      ${this.helperHTML}
     `;
-  }
 
-  updateElement(){
+    this.addEventListener('input',()=>this.changeInput());
   }
 
   changeInput(){
     console.log(this.textContent.length);
   }
 
-  connectedCallback(){
-    this.renderElement();
-  }
-
-  disconnectedCallback(){
-  }
 
 }
-customElements.define('text-input',TextInput);
+customElements.define('input-text',InputText);
