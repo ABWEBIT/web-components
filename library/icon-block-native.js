00f1adf388b4223a9d77c0e8422a3f75bc7ddafe
ABWEBIT <alexander.borzy@gmail.com>
2025-02-14 08:30:28 +0200

    update

--- library/icon-block-native.js
+++ library/icon-block-native.js
@@ -5,18 +5,18 @@ class IconBlock extends HTMLElement{
     super();
     this.attachShadow({mode:'open'});
     this.iconName = '';
-    this.iconWidth = '';
+    this.iconWidth = '20px';
+    this.iconHeight = '20px';
     this.rendered = false;
   }
-
-  static get observedAttributes(){
-    return ['name','width','height'];
-  }
+  static get observedAttributes(){return ['name','width','height'];}
 
   get iconNameFunc(){return this.iconName;}
   set iconNameFunc(value){
     this.iconName = value;
-    if(this.rendered) this.iconNameUpdate();
+    if(this.rendered){
+      this.shadowRoot.querySelector('svg').innerHTML = icons[this.iconName];
+    };
   }
 
   get iconWidthFunc(){return this.iconWidth;}
@@ -24,13 +24,20 @@ class IconBlock extends HTMLElement{
     this.iconWidth = value;
   }
 
-  style(){
+  get iconHeightFunc(){return this.iconHeight;}
+  set iconHeightFunc(value){
+    this.iconHeight = value;
+  }
+
+  css(){
     return `
     <style>
     :host{all:initial;}
     :host,:host *:not(style){box-sizing:border-box;}
 
     :host{
+      --width:20px;
+      --height:20px;
       position:relative;
       display:inline-flex;
       justify-content:center;
@@ -52,47 +59,38 @@ class IconBlock extends HTMLElement{
   html(){
     return `
       <svg viewBox="0 0 20 20">
-        ${icons[this.iconName] || ''}
+        ${icons[this.iconName]}
       </svg>
     `;
   }
 
   connectedCallback(){
     this.rendered = true;
-    this.shadowRoot.innerHTML = this.style()+this.html();
+    this.shadowRoot.innerHTML = this.css()+this.html();
   }
-
-  iconNameUpdate(){
-    let svgElement = this.shadowRoot.querySelector('svg');
-    if(svgElement){
-      svgElement.innerHTML = icons[this.iconName] || '';
-    }
-  }
-
-  disconnectedCallback(){
-  }
-
-
 
   attributeChangedCallback(name,oldValue,newValue){
     if(name === 'name'){
-
       if(newValue && oldValue !== newValue && /^[A-Za-z]+$/.test(newValue) && newValue in icons){
         this.iconNameFunc = newValue;
+        
       }
       else console.warn(`Invalid Icon Name: ${newValue}`);
-
     };
-
     if(name === 'width'){
-
       if(newValue && /^\d+(\.\d+)?(px|%)$/.test(newValue)){
         this.iconWidthFunc = newValue;
+        this.shadowRoot.host.style.setProperty('--width',newValue);
       }
       else console.warn(`Invalid Icon Width: ${newValue}`);
-
     };
-
+    if(name === 'height'){
+      if(newValue && /^\d+(\.\d+)?(px|%)$/.test(newValue)){
+        this.iconHeightFunc = newValue;
+        this.shadowRoot.host.style.setProperty('--height',newValue);
+      }
+      else console.warn(`Invalid Icon Height: ${newValue}`);
+    };
   }
 
 }
