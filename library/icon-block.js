c1458ccc4b408cc8f71789f4803767b4a343557b
ABWEBIT <alexander.borzy@gmail.com>
2025-03-09 15:20:42 +0200

    Update icon-block.js

--- library/icon-block.js
+++ library/icon-block.js
@@ -2,36 +2,38 @@ import * as icons from '../helpers/icons-pack.js';
 
 class IconBlock extends HTMLElement{
   #shadow = this.attachShadow({mode:'open'});
-  #name = '';
-  #width = '';
-  #height = '';
+  #nameData = '';
+  #widthData = '';
+  #heightData = '';
 
   static get observedAttributes(){return ['name','width','height'];}
 
-  get _name(){return this.#name;}
-  set _name(value){
-    if(/^[A-Za-z]+$/.test(value) && icons[value]){
-      this.#name = value;
+  get nameProp(){return this.#nameData;}
+  set nameProp(value){
+    if(/^[A-Za-z0-9]+$/.test(value) && icons[value]){
+      this.#nameData = value;
       const svg = this.#shadow.querySelector('svg');
-      if(svg) svg.innerHTML = icons[this.#name];
+      if(svg) svg.innerHTML = icons[this.#nameData];
     }
     else console.warn(`invalid name: ${value}`);
   }
 
-  get _width(){return this.#width;}
-  set _width(value){
+  get widthProp(){return this.#widthData;}
+  set widthProp(value){
     if(/^\d+(\.\d+)?(px|%)$/.test(value)){
-      this.#width = value;
-      this.#shadow.host.style.setProperty(`--width`,this.#width);
+      this.#widthData = value;
+      this.#shadow.host.style.setProperty(`--width`,this.#widthData);
     }
+    else console.warn(`invalid width: ${value}`);
   }
 
-  get _height(){return this.#height;}
-  set _height(value){
+  get heightProp(){return this.#heightData;}
+  set heightProp(value){
     if(/^\d+(\.\d+)?(px|%)$/.test(value)){
-      this.#height = value;
-      this.#shadow.host.style.setProperty(`--height`,this.#height);
+      this.#heightData = value;
+      this.#shadow.host.style.setProperty(`--height`,this.#heightData);
     }
+    else console.warn(`invalid height: ${value}`);
   }
 
   connectedCallback(){
@@ -57,15 +59,19 @@ class IconBlock extends HTMLElement{
       user-select:none;}
     </style>
     <svg viewBox="0 0 20 20">
-      ${icons?.[this.#name] || ''}
+      ${icons?.[this.#nameData] || ''}
     </svg>
     `;
   }
 
   attributeChangedCallback(name,oldValue,newValue){
-    if(name === 'name' && oldValue !== newValue){this._name = newValue;};
-    if(name === 'width' && oldValue !== newValue){this._width = newValue;};
-    if(name === 'height' && oldValue !== newValue){this._height = newValue;};
+    if(oldValue !== newValue){
+      switch(name){
+        case 'name':this.nameProp = newValue; break;
+        case 'width':this.widthProp = newValue; break;
+        case 'height':this.heightProp = newValue; break;
+      }
+    }
   }
 
 }
