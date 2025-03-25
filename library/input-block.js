5d3e4cb626010e59db438bb8f78f3006be7bfd5c
ABWEBIT <alexander.borzy@gmail.com>
2025-03-25 10:36:18 +0200

    update

--- library/input-block.js
+++ library/input-block.js
@@ -3,38 +3,51 @@ import uuid from '../helpers/uuid.js';
 class InputBlock extends HTMLElement{
   #before = '';
   #after = '';
-  #type = '';
+  #type = 'text';
+  #types = ['text','password','email','url','search','tel'];
+
+  constructor(){
+    super();
+    this.validator = this.validation.bind(this);
+  }
 
   static get observedAttributes(){
-    return ['before','after'];
+    return ['before','after','type'];
   }
 
   get _before(){return this.#before;}
-  set _before(value){this.#before = value;}
+  set _before(value){
+    this.#before = this.#checkName(value) ? value : '';}
 
   get _after(){return this.#after;}
-  set _after(value){this.#after = value;}
+  set _after(value){
+    this.#after = this.#checkName(value) ? value : '';}
+
+  get _type(){return this.#type;}
+  set _type(value){
+    this.#type = this.#types.includes(value) ? value : 'text';}
+
+  #checkName(name) {
+    return /^[A-Za-z][A-Za-z0-9]*$/.test(name);
+  }
 
   connectedCallback(){
-    const iconBlock = (name) => /^[A-Za-z][A-Za-z0-9]*$/.test(name) ? `<icon-block name="${name}"></icon-block>` : '';
-
     this.innerHTML = `
-      ${iconBlock(this.#before)}
-      <input>
-      ${iconBlock(this.#after)}`;
+      ${this.#before ? `<icon-block name="${this.#before}"></icon-block>` : ''}
+      <input type="${this.#type}">
+      ${this.#after ? `<icon-block name="${this.#after}"></icon-block>` : ''}`;
 
+    this.querySelector('input').addEventListener('input',this.validator);
 
     //this.setAttribute('data-uuid',uuid());
-
-    //this.shadowRoot.querySelector('.input').addEventListener('input',this.validator);
   }
 
   disconnectedCallback(){
-    //this.shadowRoot.querySelector('.input').removeEventListener('input',this.validator);
+    this.querySelector('input').removeEventListener('input',this.validator);
   }
 
   validation(){
-    //console.log(this.shadowRoot.querySelector('.input').textContent.length);
+    console.log(this.querySelector('input').value.length);
   }
 
   attributeChangedCallback(name,oldValue,newValue){
@@ -42,6 +55,7 @@ class InputBlock extends HTMLElement{
       switch(name){
         case 'before':this._before = newValue; break;
         case 'after':this._after = newValue; break;
+        case 'type':this._type = newValue; break;
       }
     }
   }
