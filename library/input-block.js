6ce0f2183512b3b5be69bba2799edd7db465b530
ABWEBIT <alexander.borzy@gmail.com>
2025-03-25 07:01:15 +0200

    update

--- library/input-block.js
+++ library/input-block.js
@@ -1,11 +1,29 @@
 import uuid from '../helpers/uuid.js';
 
 class InputBlock extends HTMLElement{
-  //#label = 'Label';
-  //#hint = 'This is the hint for input field';
-  //#validator = this.validation.bind(this);
+  #before = '';
+  #after = '';
+  #type = '';
+
+  static get observedAttributes(){
+    return ['before','after'];
+  }
+
+  get _before(){return this.#before;}
+  set _before(value){this.#before = value;}
+
+  get _after(){return this.#after;}
+  set _after(value){this.#after = value;}
 
   connectedCallback(){
+    const iconBlock = (name) => /^[A-Za-z][A-Za-z0-9]*$/.test(name) ? `<icon-block name="${name}"></icon-block>` : '';
+
+    this.innerHTML = `
+      ${iconBlock(this.#before)}
+      <input>
+      ${iconBlock(this.#after)}`;
+
+
     //this.setAttribute('data-uuid',uuid());
 
     //this.shadowRoot.querySelector('.input').addEventListener('input',this.validator);
@@ -17,6 +35,15 @@ class InputBlock extends HTMLElement{
 
   validation(){
     //console.log(this.shadowRoot.querySelector('.input').textContent.length);
+  }
+
+  attributeChangedCallback(name,oldValue,newValue){
+    if(!!newValue && oldValue !== newValue){
+      switch(name){
+        case 'before':this._before = newValue; break;
+        case 'after':this._after = newValue; break;
+      }
+    }
   }
 
 }
