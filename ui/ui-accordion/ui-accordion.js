71c6f004057fd947cfb775a318212989c65bbc0b
ABWEBIT <alexander.borzy@gmail.com>
2025-07-13 16:22:08 +0300

    update

--- ui/ui-accordion/ui-accordion.js
+++ ui/ui-accordion/ui-accordion.js
@@ -14,12 +14,12 @@ class UIAccordion extends UIBase{
   get items(){return this.#items;}
   set items(value){
     if(!Array.isArray(value)) throw new Error('Items must be an array');
-
+    this.#items = value;
+    this.render();
   }
 
   connectedCallback(){
     super.connectedCallback();
-    this.shape();
     this.size();
     this.color();
 
@@ -31,6 +31,33 @@ class UIAccordion extends UIBase{
     this.removeEventListener('click',this.#onClick);
     this.removeEventListener('keydown',this.#onKeyDown);
   }
+
+  render(){
+    this.replaceChildren();
+
+    this.#items.forEach((item,index) => {
+      const accordionItem = document.createElement('div');
+      const accordionTitle = document.createElement('div');
+      const accordionContent = document.createElement('div');
+
+      accordionItem.setAttribute('data-ui','accordion-item');
+      this.setAttributes(accordionTitle,{
+        'tabindex': '0',
+        'data-ui': 'accordion-title'
+      });
+
+      accordionContent.setAttribute('data-ui','accordion-content');
+
+      accordionTitle.textContent = item.title ?? '';
+      accordionContent.textContent = item.content ?? '';
+      
+      accordionItem.dataset.index = index;
+
+      accordionItem.append(accordionTitle,accordionContent);
+      this.appendChild(accordionItem);
+    });
+  }
+
 
   onClick(e){
     if(this.disabled) return;
--- ui/ui-version.js
+++ ui/ui-version.js
@@ -1,1 +1,1 @@
-export const ui_version = '0.1.19-alpha';
+export const ui_version = '0.1.20-alpha';
