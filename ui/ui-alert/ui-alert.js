32b96e36c37bd449ac04e0955f1a213dce00d78d
ABWEBIT <alexander.borzy@gmail.com>
2025-07-25 21:12:19 +0300

    update

--- ui/ui-alert/ui-alert.js
+++ ui/ui-alert/ui-alert.js
@@ -3,6 +3,11 @@ import {UIBase} from '../ui-base.js';
 class UIAlert extends UIBase{
   connectedCallback(){
     super.connectedCallback();
+    this.shape();
+    this.size();
+    this.theme();
+
+    this.setAttribute('role','alert')
   }
 }
 customElements.define('ui-alert',UIAlert);
