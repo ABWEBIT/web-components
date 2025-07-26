3a61a1e3818ba00d897f22c53b670ef3a857b84d
ABWEBIT <alexander.borzy@gmail.com>
2025-07-26 10:39:19 +0300

    update

--- ui/ui-alert/ui-alert.js
+++ ui/ui-alert/ui-alert.js
@@ -9,5 +9,28 @@ class UIAlert extends UIBase{
 
     this.setAttribute('role','alert')
   }
+
+  setData({label,content} = {}){
+    const wrapper = document.createElement('div');
+    wrapper.setAttribute('data-ui','alert-body');
+
+    if(label){
+      const uiAlertLabel = document.createElement('div');
+      uiAlertLabel.setAttribute('data-ui','alert-label');
+      uiAlertLabel.textContent = label;
+      wrapper.appendChild(uiAlertLabel);
+    }
+
+    if(content){
+      const uiAlertContent = document.createElement('div');
+      uiAlertContent.setAttribute('data-ui','alert-content');
+      uiAlertContent.innerHTML = content;
+      wrapper.appendChild(uiAlertContent);
+    }
+
+    this.replaceChildren(wrapper);
+  }
+
+
 }
 customElements.define('ui-alert',UIAlert);
--- ui/ui-version.js
+++ ui/ui-version.js
@@ -1,1 +1,1 @@
-export const ui_version = '0.2.07-alpha';
+export const ui_version = '0.2.08-alpha';
