dcf06c0515e54a22cdaeb853507bf139d8f2336b
ABWEBIT <alexander.borzy@gmail.com>
2026-03-07 10:39:34 +0200

    update

--- ui/components/checkbox/checkbox.js
+++ ui/components/checkbox/checkbox.js
@@ -1,6 +1,10 @@
 import {LitElement,html,nothing} from '../../lit-core.min.js';
 
 export class UICheckbox extends LitElement{
+  #children = [];
+  #isParent = null;
+  #isChild = null;
+
   static properties = {
     checked:{type:Boolean, reflect:true},
     indeterminate:{type:Boolean, reflect:true},
@@ -15,6 +19,13 @@ export class UICheckbox extends LitElement{
 
   connectedCallback(){
     super.connectedCallback();
+    this.#isParent = this.hasAttribute('parent');
+    this.#isChild = this.hasAttribute('child');
+
+    if(this.#isParent && this.#isChild){
+      console.warn('ui-checkbox cannot have both "parent" and "child" attributes',this);
+    }
+
     const options = this.getAttribute('options');
     if(!options){
       this.options = {};
@@ -45,6 +56,51 @@ export class UICheckbox extends LitElement{
   #onChange(e){
     this.checked = e.target.checked;
     this.indeterminate = false;
+
+    if(this.#isParent) this.#updateChildren();
+    else if(this.#isChild) this.#updateParent();
+  }
+
+  firstUpdated(){
+    if(this.#isParent){
+      const parentName = this.getAttribute('parent');
+      if(!parentName) return;
+
+      document.addEventListener('register-child',e =>{
+        const child = e.detail.this;
+        const childName = child.getAttribute('child');
+        if(childName === parentName && !this.#children.includes(child)){
+          this.#children.push(child);
+          const childInput = child.getElementsByTagName('input')[0];
+          childInput.addEventListener('change',() => this.#updateParent());
+          this.#updateParent();
+        }
+      });
+    }
+    else if(this.#isChild){
+      this.dispatchEvent(new CustomEvent('register-child',{
+        detail:{this:this},
+        bubbles:true,
+        composed:true
+      }));
+    }
+  }
+
+  #updateParent(){
+    if(!this.#children.length) return;
+
+    const allChecked = this.#children.every(c => c.checked);
+    const someChecked = this.#children.some(c => c.checked || c.indeterminate);
+
+    this.checked = allChecked;
+    this.indeterminate = !allChecked && someChecked;
+  };
+
+  #updateChildren(){
+    this.#children.forEach(child =>{
+      child.checked = this.checked;
+      child.indeterminate = false;
+    });
   }
 
   render(){
